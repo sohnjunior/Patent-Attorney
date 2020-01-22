fadba7a2e2d78d2ad9b64a45af39d5d44d33fc63
handhand <sohnjunior@naver.com>
2020-01-22 21:16:44 +0900

    Vue-axios & backend logic imple.

--- PatentAttorney/api/urls.py
+++ PatentAttorney/api/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'api'
 urlpatterns = [
-    # /api/patent/list
-    path('patent/list/', views.ApiPatentList.as_view(), name='patent_list'),
+    # /api/patent_image/list
+    path('patent_image/list/', views.ApiPatentList.as_view(), name='patent_list'),
 
-    # /api/patent/predict
-    path('patent/predict', views.ApiPatentPredict.as_view(), name='patent_predict'),
+    # /api/patent_image/predict
+    path('patent_image/predict', views.ApiPatentPredict.as_view(), name='patent_predict'),
 ]
