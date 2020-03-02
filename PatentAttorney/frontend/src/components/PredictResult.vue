a9eb4b40bf48759dc892a23b28f96c0938900e2a
handhand <sohnjunior@naver.com>
2020-03-02 18:37:40 +0900

    v-for key bind

--- PatentAttorney/frontend/src/components/PredictResult.vue
+++ PatentAttorney/frontend/src/components/PredictResult.vue
@@ -2,7 +2,7 @@
   <div class="app">
     <h1>유사도 측정 결과</h1>
 
-    <v-card v-for="image in images[0]" class="d-inline-block mx-auto" v-bind:key>
+    <v-card v-for="image in images[0]" class="d-inline-block mx-auto" v-bind:key="image">
       <v-container>
         <v-row justify="space-between">
           <v-col cols="auto">
