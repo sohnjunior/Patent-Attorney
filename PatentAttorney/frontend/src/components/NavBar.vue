b7de18ef006523042142a09e6f60316fdbdba4c1
sprical408 <sprical408@naver.com>
2020-03-11 01:08:23 +0900

    style

--- PatentAttorney/frontend/src/components/NavBar.vue
+++ PatentAttorney/frontend/src/components/NavBar.vue
@@ -3,8 +3,6 @@
     <router-link to="/">
       <v-toolbar-title>ImagePredict Service</v-toolbar-title>
     </router-link>
-  <v-app-bar app color="indigo" flat>
-    <v-toolbar-title>특허 이미지 검색 서비스</v-toolbar-title>
   </v-app-bar>
 </template>
 
