dcb473d18d7a6d75718b4306fe53bac521f9e997
handhand <sohnjunior@naver.com>
2020-03-18 22:15:48 +0900

    config main.js setting

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "@/plugins/vuetify";
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
+  router,
+  store,
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
