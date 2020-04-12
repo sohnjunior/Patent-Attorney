eae8d604b349627a527175ec12437cf1d4d5355c
sprical408 <sprical408@naver.com>
2020-04-13 04:52:48 +0900

    Fix page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,11 @@
 <template>
 <v-container>
   <v-footer :fixed="true" class="transparent" app>
-    <v-col class="text-center pt-0">
-      <span class="black--text">손정현 박민춘 홍종현</span>
-      <br>
+    <v-col class="text-left pt-0">
       <span class="black--text">&copy; 2020</span>
+    </v-col>
+    <v-col class="text-right pt-0">
+      <span class="black--text">손정현 박민춘 홍종현</span>
     </v-col>
   </v-footer>
   </v-container>
