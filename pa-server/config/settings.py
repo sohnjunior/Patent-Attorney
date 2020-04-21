6895026e74c56dc282257966644ceb1232a6bb1d
handhand <sohnjunior@naver.com>
2020-04-21 13:43:57 +0900

    AWS MySQL RDS 설정

--- pa-server/config/settings.py
+++ pa-server/config/settings.py
@@ -2,10 +2,8 @@ import os
 import json
 from django.core.exceptions import ImproperlyConfigured
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -22,13 +20,16 @@ def get_secret(setting, secrets=secrets):
         raise ImproperlyConfigured(error_msg)
 
 
+# -----------   SECRETKEY LIST  ----------- #
 SECRET_KEY = get_secret("SECRET_KEY")
+S3_ACCESS_KEY = get_secret("S3_ACCESS_KEY")
+S3_SECRET_KEY = get_secret("S3_SECRET_KEY")
+# ----------------------------------------- #
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -75,17 +76,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'patentattorney',
+        'USER': 'admin',
+        'PASSWORD': 'wjdgus090!',
+        'HOST': 'django-patentattorney.cxsknuyyabfv.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -105,7 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -118,7 +123,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = False
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
